fe596b80620990ff042983a92d0dd518d2df32a1
reisir <reisirito@gmail.com>
2023-11-03 20:54:59 +0200

    switch big link to wiki, use rainmeter.skin domain

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 function latest() {
-  fetch(`https://mond.amv.tools/skins?full_name=reisir/mondtholomew`)
+  fetch(`https://api.rainmeter.skin/skins?full_name=reisir/mondtholomew`)
     .then((response) => response.json())
     .then((data) => {
       const { latest_release } = data[0];
